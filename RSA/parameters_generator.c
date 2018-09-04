010e8b58409b1746fa44493fb01988869f54a5f1
NekodRider <Driver2223@gmail.com>
2018-09-05 05:49:48 +0800

    feat:finished RSA parameters generator

--- RSA/parameters_generator.c
+++ RSA/parameters_generator.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 #include <gmp.h>
+#include <time.h>
+#include <windows.h>
+
 int main()
 {
-    int i, reps = 40;
-    char num1024b[1025], num2048b[2049];
+    int i;
+    mpz_t p, q, n, a, b, phi_n;
     gmp_randstate_t state;
     gmp_randinit_default(state);
-    mpz_t p, q, n, a, b, P_MAX, N_MAX;
-    num1024b[0] = "1";
-    for (i = 1; i < 1025; i++)
-    {
-        num1024b[i] = "0";
-    }
-    num2048b[0] = "1";
-    for (i = 1; i < 2049; i++)
-    {
-        num2048b[i] = "0";
-    }
-
-    mpz_init_set_str(P_MAX, num1024b, 10);
-    mpz_init_set_str(N_MAX, num2048b, 10);
+    gmp_randseed_ui(state, time(NULL));
 
     mpz_init(p);
-    while (1)
-    {
-        mpz_urandomm(p, state, P_MAX);
-        if (mpz_probab_prime_p(p, reps) == 2)
-            break;
-        //2 is prime 1 prob. Reasonable values of reps are between 15 and 50.
-    }
-
     mpz_init(q);
-    while (1)
-    {
-        mpz_urandomm(q, state, P_MAX);
-        if (mpz_probab_prime_p(q, reps) == 2)
-            break;
-    }
+    mpz_init(n);
+    mpz_init(a);
+    mpz_init(b);
+    mpz_init(phi_n);
+
+    mpz_urandomb(p, state, 1024);
+    if (mpz_even_p(p))
+        mpz_add_ui(p, p, 1);
+    while (!mpz_probab_prime_p(p, 35) > 0)
+        mpz_add_ui(p, p, 2);
+
+    mpz_urandomb(q, state, 1024);
+    if (mpz_even_p(q))
+        mpz_add_ui(q, q, 1);
+    while (!mpz_probab_prime_p(q, 35) > 0)
+        mpz_add_ui(q, q, 2);
+
+    mpz_mul(n, p, q);
+
+    mpz_sub_ui(p, p, 1);
+    mpz_sub_ui(q, q, 1);
+    mpz_mul(phi_n, p, q);
+
+    mpz_init_set_ui(b, 65537); //公共指数b按标准为65537
+
+    mpz_invert(a, b, phi_n); //求e的数论倒数d
+
+    gmp_printf("%s (\n  %ZX,\n  %ZX\n)\n", "public key:\n", n, b);             //输出公钥(n, b)
+    gmp_printf("%s (\n  %ZX,\n  %ZX,\n  %ZX\n)\n", "private key:\n", p, q, a); //输出私钥(p,q,a)
 
     return 0;
 }
